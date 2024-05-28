347cb7c05e3f354c2c158fd9549438d28ff48f7b
Oscar White <oscar.white361@icloud.com>
2024-05-28 14:19:05 +0100

    Add project files.

---
+++ CalculatorBackend/Calculator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorBackend
+{
+    internal class Calculator
+    {
+        public static string Calculate(string userInput)
+        {
+
+            if (ValidateUserInput(userInput))
+            {
+                var equation = CreateEquation(userInput);
+            }
+            else
+            {
+                return "Invalid input";
+            }
+
+
+            return "test";
+        }
+
+        private static bool ValidateUserInput(string userInput)
+        {
+            return true;
+        }
+
+        private static bool ValidateEquation(string equation)
+        {
+            return true;
+        }
+
+        private static string[] CreateEquation(string rawEquation)
+        {
+            var 
+        }
+
+    }
+}
